441b7b5d4f3168aace47945e85b033d9bc033965
Rafael Marinato Assis <149088604+rafaelmarinatoassis@users.noreply.github.com>
2026-07-15 17:51:12 -0300

    feat: prioriza novo ícone do PWA

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,6 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#090909",
     theme_color: "#090909",
     icons: [
+      { src: "/urbtrain-app-icon.svg", sizes: "any", type: "image/svg+xml", purpose: "any maskable" },
       { src: "/pwa-splash-icon-192.png", sizes: "192x192", type: "image/png" },
       { src: "/pwa-splash-icon-512.png", sizes: "512x512", type: "image/png" },
       { src: "/pwa-splash-icon-maskable-192.png", sizes: "192x192", type: "image/png", purpose: "maskable" },
